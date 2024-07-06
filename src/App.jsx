fa51f75e4c9b9c9865f41df6737a6c91de6abf1e
ramighammami@gmail.com <ramighammami@gmail.com>
2024-07-06 07:02:04 +0300

    video is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,193 +1,89 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
 import * as THREE from 'three'
-import { useRef, useReducer, useMemo, useState, useEffect } from 'react'
-import { Environment, Lightformer, Line, OrbitControls, Points } from '@react-three/drei'
-import { BallCollider, Physics, RigidBody } from '@react-three/rapier'
+import { MeshReflectorMaterial } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { easing } from 'maath'
-import { Bloom, EffectComposer, Noise } from '@react-three/postprocessing'
-import { BlurPass, Resizer, KernelSize, Resolution, BlendFunction } from 'postprocessing'
-import * as random from 'maath/random'
-import * as buffer from 'maath/buffer'
-import * as misc from 'maath/misc'
-import complexWave from './easings/complexWave'
+import { forwardRef, useEffect, useState } from 'react'
+import { Bloom, EffectComposer, GodRays } from '@react-three/postprocessing'
 
-const accents = ['#ff4060', '#ffcc00', '#20ffa0', '#4060ff']
-const shuffle = (accent = 0) => [
-  { color: '#444', roughness: 0.1, metalness: 0.8 },
-  { color: '#444', roughness: 0.1, metalness: 0.8 },
-  { color: '#444', roughness: 0.1, metalness: 0.8 },
-  { color: 'white', roughness: 0.1, metalness: 0.1 },
-  { color: 'white', roughness: 0.1, metalness: 0.1 },
-  { color: 'white', roughness: 0.1, metalness: 0.1 },
-  { color: accents[accent], roughness: 0.1, accent: true },
-  { color: accents[accent], roughness: 0.1, accent: true },
-  { color: accents[accent], roughness: 0.1, accent: true },
-  { color: '#444', roughness: 0.1 },
-  { color: '#444', roughness: 0.3 },
-  { color: '#444', roughness: 0.3 },
-  { color: 'white', roughness: 0.1 },
-  { color: 'white', roughness: 0.2 },
-  { color: 'white', roughness: 0.1 },
-  {
-    color: accents[accent],
-    roughness: 0.1,
-    accent: true,
-    transparent: true,
-    opacity: 0.5
-  },
-  { color: accents[accent], roughness: 0.3, accent: true },
-  { color: accents[accent], roughness: 0.1, accent: true }
-]
-
-export default function App(props) {
-  // const [count, setCount] = useState(0)
-  const [accent, click] = useReducer((state) => ++state % accents.length, 0)
-  const connectors = useMemo(() => shuffle(accent), [accent])
-
+export default function App() {
   return (
-    <Canvas orthographic camera={{ zoom: 200 }}>
-      <color attach="background" args={['#000']} />
-      <PointsDemo />
-      <OrbitControls />
+    <Canvas camera={{ position: [0, 0, 30], fov: 35, near: 1, far: 60 }} gl={{ antialias: false }}>
+      <color attach="background" args={['#050505']} />
+      <ambientLight />
+      <Screen />
+
+      <Floor />
     </Canvas>
   )
 }
 
-function Sphere({
-  position,
-  children,
-  vec = new THREE.Vector3(),
-  scale,
-  r = THREE.MathUtils.randFloatSpread,
-  accent,
-  color = 'white',
-  ...props
-}) {
-  const api = useRef()
-  const ref = useRef()
-  const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
+function Rig() {
   useFrame((state, delta) => {
-    delta = Math.min(0.1, delta)
-    api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
-    easing.dampC(ref.current.material.color, color, 0.2, delta)
-  })
-  return (
-    <RigidBody linearDamping={2} angularDamping={1} friction={0.2} position={pos} ref={api} colliders={false}>
-      <BallCollider args={[1]} />
-      <mesh ref={ref} castShadow receiveShadow>
-        <sphereGeometry args={[1, 64, 64]} />
-        <meshStandardMaterial toneMapped={false} emissiveIntensity={0.2} {...props} />
-        {children}
-      </mesh>
-    </RigidBody>
-  )
-}
-
-function Pointer({ vec = new THREE.Vector3() }) {
-  const ref = useRef()
-  useFrame(({ pointer, viewport }) =>
-    ref.current?.setNextKinematicTranslation(
-      vec.set((pointer.x * viewport.width) / 2, (pointer.y * viewport.height) / 2, 0)
+    easing.damp3(
+      state.camera.position,
+      [5 + state.pointer.x, 0 + +state.pointer.y, 18 + Math.atan2(state.pointer.x, state.pointer.y) * 2],
+      0.4,
+      delta
     )
-  )
-  return (
-    <RigidBody position={[5, 5, 5]} type="kinematicPosition" colliders={true} ref={ref}>
-      <BallCollider args={[2]} />
-    </RigidBody>
-  )
-}
-
-function Effects() {
-  return (
-    <EffectComposer>
-      <Bloom
-        intensity={0.1} // The bloom intensity.
-        blurPass={undefined} // A blur pass.
-        kernelSize={KernelSize.VERY_LARGE} // blur kernel size
-        luminanceThreshold={0.5} // luminance threshold. Raise this value to mask out darker elements in the scene.
-        luminanceSmoothing={0.2} // smoothness of the luminance threshold. Range is [0, 1]
-        mipmapBlur={true} // Enables or disables mipmap blur.
-        resolutionX={Resolution.AUTO_SIZE} // The horizontal resolution.
-        resolutionY={Resolution.AUTO_SIZE} // The vertical resolution.
-      />
-      <Noise
-        premultiply // enables or disables noise premultiplication
-        blendFunction={BlendFunction.HUE} // blend mode
-      />
-    </EffectComposer>
-  )
-}
-
-function Env() {
-  return (
-    <Environment resolution={256}>
-      <group rotation={[-Math.PI / 3, 0, 1]}>
-        <Lightformer form="circle" intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={2} />
-        <Lightformer form="circle" intensity={25} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={2} />
-        <Lightformer form="circle" intensity={25} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={2} />
-        <Lightformer form="circle" intensity={25} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={2} />
-        <Lightformer
-          form="ring"
-          color="#4060ff"
-          intensity={80}
-          onUpdate={(self) => self.lookAt(0, 0, 0)}
-          position={[10, 10, 0]}
-          scale={10}
-        />
-      </group>
-    </Environment>
-  )
-}
-
-const rotationAxis = new THREE.Vector3(0.2, 1, 0.5).normalize()
-const q = new THREE.Quaternion()
-
-function PointsDemo(props) {
-  const pointsRef = useRef()
-  // export declare function inRect<T extends TypedArray>(buffer: T, rect?: Rect, rng?: Generator): T;
-
-  useEffect(() => {
-    console.log('🚀 ~ useEffect ~ pointsRef.current:', pointsRef.current)
-  }, [])
-  const [{ box, sphere, final }] = useState(() => {
-    const box = random.inBox(new Float32Array(10000 * 3), { side: 3 })
-    const sphere = random.inSphere(new Float32Array(10000 * 3), { radius: 0.75 })
-    const final = box.slice(0) // final buffer that will be used for the points mesh
-    return { box, sphere, final }
+    state.camera.lookAt(0, 0, 0)
   })
+}
 
-  // export declare function inSphere(buffer: TypedArray, sphere?: Sphere, rng?: Generator): TypedArray;
+const Floor = () => (
+  <mesh position={[0, -5.02, 0]} receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+    <planeGeometry args={[50, 50]} />
+    <MeshReflectorMaterial
+      blur={[300, 50]}
+      resolution={1024}
+      mixBlur={1}
+      mixStrength={100}
+      roughness={1}
+      depthScale={1.2}
+      minDepthThreshold={0.4}
+      maxDepthThreshold={1.4}
+      color="#202020"
+      metalness={0.8}
+    />
+  </mesh>
+)
 
-  useFrame(({ clock }) => {
-    const et = clock.getElapsedTime()
-    const delta = clock.getDelta()
-    const t = misc.remap(Math.sin(et), [-1, 1], [0, 1])
-    const t2 = misc.remap(Math.cos(et * 1), [-1, 1], [0, 1])
-    const t3 = misc.remap(complexWave(et), [-1, 1], [0, 1])
-    const t4 = easing.linear(t)
-
-    // pointsRef.current.material.color.setHSL(t, 3, 0.2)
-    // change material color with dampC
-    // easing.dampC(pointsRef.current.material.color, { r: 0.2, g: 0.2, b: 0.5 }, 0.25, delta)
-
-    buffer.rotate(sphere, {
-      q: q.setFromAxisAngle(rotationAxis, t4 * 0.05)
+const Emitter = forwardRef((props, forwardRef) => {
+  const [video] = useState(() =>
+    Object.assign(document.createElement('video'), {
+      src: '/duvas.MOV',
+      crossOrigin: 'Anonymous',
+      loop: true,
+      muted: true
     })
-    buffer.swizzle(box, 4, 'xzy')
-    buffer.lerp(box, sphere, final, t4)
-  })
-  // export declare function swizzle(buffer: TypedArray, stride?: number, swizzle?: string): TypedArray;
-
+  )
+  useEffect(() => void video.play(), [video])
   return (
-    <Points
-      positions={final}
-      stride={3}
-      //    ref={pointsRef}
-      {...props}>
-      <pointsMaterial size={1} blending={2} />
-    </Points>
+    <mesh ref={forwardRef} position={[0, 0, -16]} {...props}>
+      <planeGeometry args={[16, 10]} />
+      <meshBasicMaterial>
+        <videoTexture attach="map" args={[video]} colorSpace={THREE.SRGBColorSpace} />
+      </meshBasicMaterial>
+      <mesh scale={[16.05, 10.05, 1]} position={[0, 0, -0.01]}>
+        <planeGeometry />
+        <meshBasicMaterial color="black" />
+      </mesh>
+    </mesh>
+  )
+})
+Emitter.displayName = 'Emitter'
+
+function Screen() {
+  const [material, set] = useState()
+  return (
+    <>
+      <Emitter ref={set} />
+      {material && (
+        <EffectComposer disableNormalPass multisampling={8}>
+          <GodRays sun={material} exposure={0.34} decay={0.8} blur />
+          <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={1} />
+        </EffectComposer>
+      )}
+    </>
   )
 }
